b28eeb568762dec581dd1098e1fcc02fa828f1c3
nbaruah <nayanmanibaruah@gmail.com>
2019-07-18 17:22:02 +0530

    create registration form view

---
+++ application/views/user/registration.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row">
+		<form action="#">
+			<div class="form-group">
+			<label>Name</label>
+			<input type="text" name="c_fname" class="form-control>
+			</div>
+		</form>
+	</div>
+</div>
