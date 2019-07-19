818b984f4a1fc7802d4302bcb0bf6eb2883021c2
nbaruah <nayanmanibaruah@gmail.com>
2019-07-19 08:37:13 +0530

    Modified user controller

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,17 +1,9 @@
 <?php
-/**
- *
- * @author nbaruah
- *        
- */
 class User extends CI_Controller
 {
-    public function __construct(){
-        $this->load->helper("html");
-    }
-    
     public function index(){
         $this->load->view("user/registration");
     }
 }
 
+?>
