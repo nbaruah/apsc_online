462aa7ec482faed14aadaad653664c4b86e9f204
nbaruah <nayanmanibaruah@gmail.com>
2019-07-19 18:05:57 +0530

    Front end design of registration form adding Candidate's, father's and Mother's Name

--- application/views/user/registration.php
+++ application/views/user/registration.php
@@ -36,15 +36,75 @@
     </div>
 </nav>
 
-	<div class="container">
-		<div class="row">
+	<div class="container" style="box-shadow: 0 0 0 2px black;" >
 			<form action="#">
-				<div class="form-group">
-					<label>Name</label> 
-					<input type="text" name="c_fname" class="form-control">
-				</div>
+  <!-- Candidate's Name -->
+  <div class="form-row"> 
+    <label class="col-sm-2 col-form-label col-form-label-sm">Candidate's Name</label>
+    <div class="form-group col-sm-4">
+      <input type="text" class="form-control form-control-sm" name="c_fname" placeholder="First Name">
+    </div>
+    <div class="form-group col-sm-3">
+      <input type="text" class="form-control form-control-sm" name="c_mname" placeholder="Middle Name">
+    </div>
+    <div class="form-group col-sm-3">
+      <input type="text" class="form-control form-control-sm" name="c_lname" placeholder="Last Name">
+    </div>
+  </div>
+
+  <!-- Father's Name -->
+  <div class="form-row"> 
+    <label class="col-sm-2 col-form-label col-form-label-sm">Father's Name</label>
+    <div class="form-group col-sm-4">
+      <input type="text" class="form-control form-control-sm" name="f_fname" placeholder="First Name">
+    </div>
+    <div class="form-group col-sm-3">
+      <input type="text" class="form-control form-control-sm" name="f_mname" placeholder="Middle Name">
+    </div>
+    <div class="form-group col-sm-3">
+      <input type="text" class="form-control form-control-sm" name="f_lname" placeholder="Last Name">
+    </div>
+  </div>
+
+  <!-- Mother's Name -->
+  <div class="form-row"> 
+    <label class="col-sm-2 col-form-label col-form-label-sm">
+    	Mother's Name <small class="text-success">(Please Do not use Shri/Mr./Ms./Dr./Mrs. etc.)</small>
+    </label>
+    <div class="form-group col-sm-4">
+      <input type="text" class="form-control form-control-sm" name="m_fname" placeholder="First Name">
+    </div>
+    <div class="form-group col-sm-3">
+      <input type="text" class="form-control form-control-sm" name="m_mname" placeholder="Middle Name">
+    </div>
+    <div class="form-group col-sm-3">
+      <input type="text" class="form-control form-control-sm" name="m_lname" placeholder="Last Name">
+    </div>
+  </div>
+
+<div class="form-row">
+  <label class="col-form-label col-form-label-sm col-sm-2">Gender</label>
+  <div class="form-group col-sm-2">
+    <select class="form-control form-control-sm">
+      <option>Small select</option>
+    </select>
+  </div>
+
+  <label class="col-form-label col-form-label-sm col-sm-2"> Date of Birth</label>
+  <div class="form-group col-sm-2">
+    <input class="form-control form-control-sm" type="text" name="dob" placeholder="Date of Birth">
+  </div>
+
+  <label class="col-form-label col-form-label-sm col-sm-2">Community</label>
+  <div class="form-group col-sm-2">
+    <select class="form-control form-control-sm">
+      <option>Small select</option>
+    </select>
+  </div>
+</div>
+
+
 			</form>
-		</div>
 	</div>
 
 </body>
