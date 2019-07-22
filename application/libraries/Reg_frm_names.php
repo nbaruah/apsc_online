2c8c7b4d560b0f23af5e11de54d3ddbddbf72633
nbaruah <nayanmanibaruah@gmail.com>
2019-07-22 18:01:41 +0530

    Added a Class of constants for Candidate Registration form

---
+++ application/libraries/Reg_frm_names.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Reg_frm_names {
+    const C_FNAME = 'c_fname';
+    const C_MNAME = 'c_mname';
+    const C_LNAME = 'c_lname';
+    const F_FNAME = 'f_fname';
+    const F_MNAME = 'f_mname';
+    const F_LNAME = 'f_lname';
+    const M_FNAME = 'm_fname';
+    const M_MNAME = 'm_mname';
+    const M_LNAME = 'm_lname';
+    
+    
+    public function c_fname()
+    {
+        return self::C_FNAME;
+    }
+}
+
+?>
