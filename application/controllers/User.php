2f2904c35b650a16f52923b506e6b18c9f8810e6
nbaruah <nayanmanibaruah@gmail.com>
2019-07-25 09:21:32 +0530

    loaded the custom library to be used in reg form

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,6 +2,8 @@
 class User extends CI_Controller
 {
     public function index(){
+    	//Loaded this library to be used in the view for name attributes
+    	$this->load->library('Reg_frm_names', NULL, 'reg_frm');
         $this->load->view("user/registration");
     }
 }
