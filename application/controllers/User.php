b77c571a92680fdd002d2bc6fe2d09a75bfed333
nbaruah <nayanmanibaruah@gmail.com>
2019-07-25 17:37:58 +0530

    created new controller method for processing registration

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,10 +1,18 @@
 <?php
 class User extends CI_Controller
 {
+    
+    
+    
     public function index(){
     	//Loaded this library to be used in the view for name attributes
     	$this->load->library('Reg_frm_names', NULL, 'reg_frm');
+    	$this->load->helper('form');
         $this->load->view("user/registration");
+    }
+    
+    public function register(){
+       echo 'welcome';
     }
 }
 
